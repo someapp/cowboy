ee51de1999ef050f4e5d15184234ef8e550f7dd3
root <root@chat.dev0.spark.net>
2013-08-29 16:45:14 -0400

    start script

--- examples/ejabberd_rest_api/start.sh
+++ examples/ejabberd_rest_api/start.sh
@@ -1,3 +1,2 @@
 #!/bin/sh_
-erl -pa ebin deps/*/ebin -s ejabberd_rest_api \
-	-eval "io:format(\"Point your browser at http://localhost:8080/~n\")."
+erl -pa ebin deps/*/ebin -s ejabberd_rest_api start
