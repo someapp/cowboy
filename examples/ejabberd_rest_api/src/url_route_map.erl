9a3081448c7433091baf10445ffb1bc7f076b043
root <root@chat.dev0.spark.net>
2013-09-16 13:41:09 -0400

    mispelt

--- examples/ejabberd_rest_api/src/url_route_map.erl
+++ examples/ejabberd_rest_api/src/url_route_map.erl
@@ -24,7 +24,7 @@ route_map(Host, Opts, ConfigHosts)->
 			{"/online/user/", ejabberd_online_user_handler, Opts},
 			{"/online/user/total/", 
 			     ejabberd_online_user_set_handler, Opts},
-			{"/*", cowboy_static, {fun mimetypes:path_to_mime/2, default}}
+			{"/*", cowboy_static, {fun mimetypes:path_to_mimes/2, default}}
 			
 		]
 	}
